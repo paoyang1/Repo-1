e03a403481ee7f5dcb43f9ceb76b883f1fcbe3e2
Pao Yang <paoyang@uark.edu>
2019-02-05 15:14:01 -0600

    tracker 1. Adding who is saying hello

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,5 +3,6 @@ class HelloWorldApp
   public static void main (String[] args)
   {
     System.out.println("Hello World!");
+    System.out.println("Says George."); // Tracker 1- Display hello for george
   }
 }
