bf645c48b196f878bb0de5022ac301d82926af30
Pao Yang <paoyang@uark.edu>
2019-02-05 17:15:09 -0600

    Tracker 2. Changes to be more inclusive

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,5 +3,6 @@ class HelloWorldApp
   public static void main (String[] args)
   {
     System.out.println("Hello World!");
+    System.out.println("I am Janice, and Hi George you messed up"); // Tracker 2
   }
 }
